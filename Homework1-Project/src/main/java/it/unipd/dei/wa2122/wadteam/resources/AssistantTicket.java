15e34f17582ac764118096c1a6dd76cd0bc70abf
Gianpietro Nicoletti <gianpietro.nicoletti@studenti.unipd.it>
2022-03-26 15:16:07 +0100

    Added AssistantTicket pojo

---
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/resources/AssistantTicket.java
@@ -0,0 +1,44 @@
+package it.unipd.dei.wa2122.wadteam.resources;
+
+import org.json.JSONObject;
+
+public class AssistantTicket implements Resource {
+
+    private final int id;
+    private final String description;
+    private final int idCustomer;
+    private final String productAlias;
+
+    public AssistantTicket(int id, String description, int idCustomer, String productAlias) {
+        this.id = id;
+        this.description = description;
+        this.idCustomer = idCustomer;
+        this.productAlias = productAlias;
+    }
+
+    public final int getId() {
+        return id;
+    }
+
+    public final String getDescription() {
+        return description;
+    }
+
+    public final int getIdCustomer() {
+        return idCustomer;
+    }
+
+    public final String getProductAlias() {
+        return productAlias;
+    }
+
+    @Override
+    public JSONObject toJSON() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id",id);
+        jsonObject.put("description",description);
+        jsonObject.put("idCustomer",idCustomer);
+        jsonObject.put("productAlias", productAlias);
+        return jsonObject;
+    }
+}
