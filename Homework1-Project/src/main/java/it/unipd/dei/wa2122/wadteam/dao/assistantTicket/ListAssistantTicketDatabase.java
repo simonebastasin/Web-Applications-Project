ba3c5797640f8fd4e97e1db89140f17577d0362d
Gianpietro Nicoletti <gianpietro.nicoletti@studenti.unipd.it>
2022-03-26 17:20:22 +0100

    Added list and update ticket status dao

---
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/dao/assistantTicket/ListAssistantTicketDatabase.java
@@ -0,0 +1,77 @@
+package it.unipd.dei.wa2122.wadteam.dao.assistantTicket;
+
+import it.unipd.dei.wa2122.wadteam.resources.AssistantTicket;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListAssistantTicketDatabase {
+    /**
+     * The SQL statement to be executed
+     */
+    private static final String STATEMENT = "SELECT * FROM Assistant_Ticket";
+
+    /**
+     * The connection to the database
+     */
+    private final Connection con;
+
+    /**
+     * Creates a new object for reading an assistant ticket.
+     *
+     * @param con
+     *            the connection to the database.
+     */
+    public ListAssistantTicketDatabase(final Connection con) {
+        this.con = con;
+    }
+
+    /**
+     * Reads an asssistant ticket from the database.
+     *
+     * @return the {@code AssistantTicket} object matching the badge.
+     *
+     * @throws SQLException
+     *             if any error occurs while reading the assistant ticket.
+     */
+    public List<AssistantTicket> getAssistantTicket() throws SQLException {
+
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        // the current employee
+        AssistantTicket resultAssistantTicketItem = null;
+
+        List<AssistantTicket> resultAssistantTicket = new ArrayList<>();
+        try {
+            preparedStatement = con.prepareStatement(STATEMENT);
+
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+
+                resultAssistantTicketItem = new AssistantTicket(resultSet.getInt("ID"),
+                        resultSet.getString("Description"),
+                        resultSet.getInt("ID_Customer"),
+                        resultSet.getString("Product_Alias"));
+                resultAssistantTicket.add(resultAssistantTicketItem);
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        }
+
+        return resultAssistantTicket;
+    }
+}
+
+
---
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/dao/assistantTicket/UpdateAssistantTicketDatabase.java
@@ -0,0 +1,81 @@
+package it.unipd.dei.wa2122.wadteam.dao.assistantTicket;
+
+import it.unipd.dei.wa2122.wadteam.resources.AssistantTicket;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UpdateAssistantTicketDatabase {
+    /**
+     * The SQL statement to be executed
+     */
+    private static final String STATEMENT = "UPDATE Employee SET Description = ?, ID_customer = ?, Product_Alias = ? WHERE ID = ?";
+
+    /**
+     * The connection to the database
+     */
+    private final Connection con;
+
+    /**
+     * The item of the assistant ticket
+     */
+    private final AssistantTicket assistantTicket;
+
+    /**
+     * Update  a assistant ticket item.
+     *
+     * @param con
+     *            the connection to the database.
+     * @param assistantTicket
+     *            the assistant ticket  to be update.
+     */
+    public UpdateAssistantTicketDatabase(final Connection con, final AssistantTicket assistantTicket) {
+        this.con = con;
+        this.assistantTicket = assistantTicket;
+    }
+
+    /**
+     * Reads an employee from the database.
+     *
+     * @return the {@code AssitantTicket} object matching the badge.
+     *
+     * @throws SQLException
+     *             if any error occurs while reading the employee.
+     */
+    public AssistantTicket getAssistantTicket() throws SQLException {
+
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        // the read employee
+        AssistantTicket resultAssistantTicket = null;
+
+        try {
+            preparedStatement = con.prepareStatement(STATEMENT);
+            preparedStatement.setString(2, assistantTicket.getDescription());
+            preparedStatement.setInt(3, assistantTicket.getIdCustomer());
+            preparedStatement.setString(4, assistantTicket.getProductAlias());
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                resultAssistantTicket = new AssistantTicket(resultSet.getInt("ID"),
+                        resultSet.getString("Description"),
+                        resultSet.getInt("ID_Customer"), 
+                        resultSet.getString("Product_Alias"));
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        }
+
+        return resultAssistantTicket;
+    }
+}
