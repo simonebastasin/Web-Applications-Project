8e1408c5a0017736591941e04d346c3a941f5ef5
Matteo <landomatteo@yahoo.it>
2022-03-26 17:15:15 +0100

    Added Discount pojo
    Added Discount pojo

---
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/resources/Discount.java
@@ -0,0 +1,58 @@
+
+import java.util.Date;
+
+import org.json.JSONObject;
+
+public class Discount implements Resource {
+    private final Integer id;
+    private final Integer percentage;
+    private final String startDate;//TODO : convert to Date
+    private final String endDate;//TODO : convert to Date
+
+    public Discount(Integer id, Integer percentage, String startDate, String endDate) {
+        this.id = id;
+        this.percentage = percentage;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getPercentage() {
+        return percentage;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    @Override
+    public JSONObject toJSON() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", id);
+        jsonObject.put("percentage", percentage);
+        jsonObject.put("startDate", startDate);
+        jsonObject.put("endDate", endDate);
+        return jsonObject;
+    }
+
+    public static Discount fromJson(JSONObject jsonObject) {
+        Integer id = jsonObject.getInt("id");
+        Integer percentage = jsonObject.getInt("percentage");
+        String startDate = jsonObject.getString("startDate");
+        String endDate = jsonObject.getString("endDate");
+        //TODO getDate JSON
+        /*
+        String dateStr = jsonObject.getString("startDate");
+        Si sdf = new Date("yyyy-MM-dd HH:mm:ss");
+        Date birthDate = sdf.parse(dateStr);
+         */
+        return new Discount(id, percentage, startDate, endDate);
+    }
+}
