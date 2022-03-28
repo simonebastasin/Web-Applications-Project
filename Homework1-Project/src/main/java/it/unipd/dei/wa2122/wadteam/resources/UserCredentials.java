fbc9fb48e3f26462429c7b1a1f98b5c6569fa45b
Gil Czaczkes <gil.czaczkes@studenti.unipd.it>
2022-03-28 17:02:49 +0200

    Added UserCredentials pojo and login.html

---
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/resources/UserCredentials.java
@@ -0,0 +1,43 @@
+package it.unipd.dei.wa2122.wadteam.resources;
+
+import org.json.JSONObject;
+
+public class UserCredentials implements Resource {
+
+    private final String identification;
+    private final String password;
+    private final TypeUser type;
+    private final String role;
+
+     public UserCredentials(String identification, String password, TypeUser type, String role) {
+        this.identification = identification;
+        this.role=role;
+        this.password = password;
+        this.type = type;
+    }
+
+    public String getIdentification() {
+        return identification;
+    }
+
+    public final String getPassword() {
+        return password;
+    }
+
+    public final TypeUser getType(){
+        return type;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public JSONObject toJSON() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("identification", identification);
+        jsonObject.put("password", password);
+        jsonObject.put("type", type);
+        return jsonObject;
+    }
+    public enum TypeUser {EMPLOYEE, CUSTOMER}
+}
