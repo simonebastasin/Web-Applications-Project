fec522605abeeb05cb4f75da36992556b73ef3b4
matteo pozzer <matteo.pozzer@studenti.unipd.it>
2022-03-28 12:28:19 +0200

    add template class Customer

---
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/resources/Customer.java
@@ -0,0 +1,17 @@
+package it.unipd.dei.wa2122.wadteam.resources;
+
+import org.json.JSONObject;
+
+public class Customer implements Resource {
+
+    final int id;
+    final String name;
+    final String surname;
+    final String fiscal_code;
+    final String address;
+    final String email;
+    final String phone_number;
+    final String username;
+    final String password;
+
+}
