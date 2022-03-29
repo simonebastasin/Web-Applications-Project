64aee23e6757f940fce93a7bc3dbb9ade2bdc87f
matteo pozzer <matteo.pozzer@studenti.unipd.it>
2022-03-29 18:28:05 +0200

    add GetIdCustomerDatabase

--- Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/dao/customer/GetIdCustomerDatabase.java
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/dao/customer/GetIdCustomerDatabase.java
@@ -1,6 +1,7 @@
 package it.unipd.dei.wa2122.wadteam.dao.customer;
 
 import java.sql.Connection;
+import java.sql.Connection;
 import it.unipd.dei.wa2122.wadteam.resources.Customer;
 
 
@@ -8,39 +9,30 @@ import it.unipd.dei.wa2122.wadteam.resources.Customer;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-public class CreateCustomerDatabase {
+public class GetIdCustomerDatabase {
 
-    private static final String STATEMENT = "INSERT INTO Customer (id,name,surname,fiscal_code,address,email,phone_number,username,password)" +
-            " VALUES (?,?, ?, ?, ?,?,?,?, sha384(?::bitea))" +
-            " RETURNING id,name,surname,fiscal_code,address,email,phone_number,username";
+    private static final String STATEMENT = "SELECT id,name,surname,fiscal_code,address,email,phone_number,username" +
+            " FROM Customer WHERE username = ?";
 
     private final Connection con;
-    private final Customer customer;
+    private final String username;
 
-    public CreateCustomerDatabase(final Connection con, final Customer customer) {
+    public GetIdCustomerDatabase(final Connection con, final String username) {
         this.con = con;
-        this.customer=customer;
+        this.username=username;
     }
-
-    public Customer createCustomer() throws SQLException {
+    public Customer getIdCustomer() throws SQLException {
 
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
 
-        // the create employee
-       Customer resultCustomer = null;
+
+        Customer resultCustomer = null;
 
         try {
             preparedStatement = con.prepareStatement(STATEMENT);
-            preparedStatement.setInt(1, customer.getId());
-            preparedStatement.setString(2, customer.getName());
-            preparedStatement.setString(3, customer.getSurname());
-            preparedStatement.setString(4, customer.getFiscal_code());
-            preparedStatement.setString(5, customer.getAddress());
-            preparedStatement.setString(6, customer.getEmail());
-            preparedStatement.setString(7, customer.getPhone_number());
-            preparedStatement.setString(8, customer.getUsername());
-            preparedStatement.setString(9, customer.getPassword());
+            preparedStatement.setString(1,username);
+
 
             resultSet = preparedStatement.executeQuery();
 
@@ -69,6 +61,4 @@ public class CreateCustomerDatabase {
 
         return resultCustomer;
     }
-
-
 }
