8c3dce3bc9f09428913c886a57d25993f9b944d1
Gioele Ceccon <gioele.ceccon@studenti.unipd.it>
2022-04-01 17:03:39 +0200

    Added GetProductByCategoryDatabase

---
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/dao/product/GetProductByCategoryDatabase.java
@@ -0,0 +1,104 @@
+package it.unipd.dei.wa2122.wadteam.dao.product;
+
+import it.unipd.dei.wa2122.wadteam.resources.Product;
+import it.unipd.dei.wa2122.wadteam.resources.ProductCategory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetProductByCategoryDatabase {
+    private static final String STATEMENT = "SELECT product_alias, name, brand, description, purchase_price, sale_price, quantity, category_name, evidence FROM product WHERE category_name = ?";
+
+    private static final String STATEMENT_GET_PICTURE = "SELECT id_media FROM Rappresented_by WHERE product_alias = ?";
+
+    /**
+     * The connection to the database
+     */
+    private final Connection con;
+
+    /**
+     * The product_alias of the product to be retrieved
+     */
+    private final String category;
+
+    /**
+     * Creates a new object for getting a product.
+     *
+     * @param con     the connection to the database.
+     * @param category the product.
+     */
+    public GetProductByCategoryDatabase(final Connection con, final String category) {
+        this.con = con;
+        this.category = category;
+    }
+
+    /**
+     * Gets a product from the database.
+     *
+     * @return the {@code Product} object matching the product_alias.
+     * @throws SQLException if any error occurs while retrieving the product.
+     */
+    public List<Product> getProductByCategory() throws SQLException {
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        Product resultProductItem = null;
+        List<Product> resultProduct = new ArrayList<>();
+
+        try {
+            preparedStatement = con.prepareStatement(STATEMENT);
+
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                resultProductItem = new Product(
+                        resultSet.getString("product_alias"),
+                        resultSet.getString("name"),
+                        resultSet.getString("brand"),
+                        resultSet.getString("description"),
+                        resultSet.getInt("quantity"),
+                        resultSet.getDouble("purchase_price"),
+                        resultSet.getDouble("sale_price"),
+                        new ProductCategory(resultSet.getString("category_name"), null),
+                        resultSet.getBoolean("evidence"),
+                        new ArrayList<>());
+                resultProduct.add(resultProductItem);
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        }
+
+        for (var product : resultProduct) {
+            try {
+                preparedStatement = con.prepareStatement(STATEMENT_GET_PICTURE);
+                preparedStatement.setString(1, product.getAlias());
+
+                resultSet = preparedStatement.executeQuery();
+
+                while (resultSet.next()) {
+                    product.getPicture().add(resultSet.getInt("id_media"));
+                }
+            } finally {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            }
+        }
+
+        return resultProduct;
+    }
+}
