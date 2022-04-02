e93b2b4277a1de9286663b639abbe4629ca7dde8
Gianpietro Nicoletti <gianpietro.nicoletti@studenti.unipd.it>
2022-04-02 11:38:27 +0200

    Added ticketStatusList into AssistantTicket

--- Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/resources/AssistantTicket.java
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/resources/AssistantTicket.java
@@ -2,18 +2,24 @@ package it.unipd.dei.wa2122.wadteam.resources;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+import it.unipd.dei.wa2122.wadteam.resources.TicketStatus;
+
 public class AssistantTicket implements Resource {
 
     private final int id;
     private final String description;
     private final int idCustomer;
     private final String productAlias;
+    private final List<TicketStatus> ticketStatusList;
 
-    public AssistantTicket(int id, String description, int idCustomer, String productAlias) {
+    public AssistantTicket(int id, String description, int idCustomer, String productAlias, List<TicketStatus> ticketStatusList) {
         this.id = id;
         this.description = description;
         this.idCustomer = idCustomer;
         this.productAlias = productAlias;
+        this.ticketStatusList = ticketStatusList;
     }
 
     public final int getId() {
@@ -32,6 +38,10 @@ public class AssistantTicket implements Resource {
         return productAlias;
     }
 
+    public final List<TicketStatus> getTicketStatusList() {
+        return ticketStatusList;
+    }
+
     @Override
     public JSONObject toJSON() {
         JSONObject jsonObject = new JSONObject();
@@ -39,6 +49,7 @@ public class AssistantTicket implements Resource {
         jsonObject.put("description",description);
         jsonObject.put("idCustomer",idCustomer);
         jsonObject.put("productAlias", productAlias);
+        jsonObject.put("ticketStatuList",ticketStatusList.stream().map(TicketStatus::toJSON).toArray());
         return jsonObject;
     }
 
@@ -47,6 +58,10 @@ public class AssistantTicket implements Resource {
         String description = jsonObject.getString("description");
         int idCustomer = jsonObject.getInt("idCustomer");
         String productAlias = jsonObject.getString("productAlias");
-        return new AssistantTicket(id, description, idCustomer, productAlias);
+        List<TicketStatus> ticketStatusList = new ArrayList<>();
+        for(var item : jsonObject.getJSONArray("ticketStatusList")) {
+            ticketStatusList.add(TicketStatus.fromJSON((JSONObject) item));
+        }
+        return new AssistantTicket(id, description, idCustomer, productAlias,ticketStatusList);
     }
 }
