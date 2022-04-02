bf52003ce32be83c84af2d1399f72efd86dfd392
Davide <davide.colussi.99@gmail.com>
2022-04-02 13:38:39 +0200

    OrderStatus added dao: DeleteOrderStatusDatabase

---
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/dao/orderStatus/DeleteOrderStatusDatabase.java
@@ -0,0 +1,73 @@
+package it.unipd.dei.wa2122.wadteam.dao.orderStatus;
+
+import it.unipd.dei.wa2122.wadteam.resources.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DeleteOrderStatusDatabase {
+    /**
+     * The SQL statement to be executed
+     */
+    private static final String STATEMENT = "DELETE FROM order_status WHERE id = ? RETURNING id, status, description, os_datetime, id_order";
+
+    /**
+     * The connection to the database
+     */
+    private final Connection con;
+
+    /**
+     * The id of the ticket status
+     */
+    private final int id;
+
+    public DeleteOrderStatusDatabase(final Connection con, final int id) {
+        this.con = con;
+        this.id = id;
+    }
+
+    /**
+     * delete OrderStatus from database with matching id
+     * @return the {@code OrderStatus} object matching the id, or null if no match has been found.
+     *
+     * @throws SQLException
+     *             if any error occurs while deleting the OrderStatus.
+     */
+    public OrderStatus deleteOrderStatus() throws SQLException {
+
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        // the deleted employee
+        OrderStatus resultOrderStatus = null;
+
+        try {
+            preparedStatement = con.prepareStatement(STATEMENT);
+            preparedStatement.setInt(1, id);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                resultOrderStatus = new OrderStatus(
+                        resultSet.getInt("id"),
+                        OrderStatusEnum.valueOf(resultSet.getString("status")),
+                        resultSet.getString("description"),
+                        resultSet.getString("os_datetime"),
+                        resultSet.getInt("id_order")
+                );
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+        }
+
+        return resultOrderStatus;
+    }
+}
