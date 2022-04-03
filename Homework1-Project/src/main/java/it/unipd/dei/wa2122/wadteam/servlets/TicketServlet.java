63d74dd378dffb923262f3ca361999c8eabda78c
Gil Czaczkes <gil.czaczkes@studenti.unipd.it>
2022-04-03 15:43:29 +0200

    fixes

--- Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/servlets/TicketServlet.java
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/servlets/TicketServlet.java
@@ -1,5 +1,6 @@
 package it.unipd.dei.wa2122.wadteam.servlets;
 
+import it.unipd.dei.wa2122.wadteam.dao.assistanceTicket.CreateAssistanceTicketDatabase;
 import it.unipd.dei.wa2122.wadteam.dao.assistanceTicket.GetAssistanceTicketDatabase;
 import it.unipd.dei.wa2122.wadteam.resources.AssistanceTicket;
 import it.unipd.dei.wa2122.wadteam.resources.Message;
@@ -43,14 +44,14 @@ public class TicketServlet extends AbstractDatabaseServlet {
         } else {
             int id = Integer.parseInt(req.getParameter("identification"));
 
-        try {
-            AssistanceTicket assistantTicket = new GetAssistanceTicketDatabase(getDataSource().getConnection(), id).getAssistanceTicket();
+            try {
+                AssistanceTicket assistanceTicket = new GetAssistanceTicketDatabase(getDataSource().getConnection(), id).getAssistanceTicket();
 
-            writeResource(req, resp, "/jsp/ticket.jsp",assistantTicket);
-        } catch (SQLException e) {
-            writeError(req, resp, new Message("Error get", "ET02", e.getMessage()), HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                writeResource(req, resp, "/jsp/ticket.jsp", true, assistanceTicket);
+            } catch (SQLException e) {
+                writeError(req, resp, new Message("Error get", "ET02", e.getMessage()), HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            }
+
         }
-
-
     }
 }
