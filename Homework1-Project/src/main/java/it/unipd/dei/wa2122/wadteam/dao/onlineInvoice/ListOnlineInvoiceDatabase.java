3de49ef4ccf797bd3ef8f4a88f30baf94871cc79
Gianpietro Nicoletti <gianpietro.nicoletti@studenti.unipd.it>
2022-04-09 16:51:10 +0200

    Added ListOnlineInvoiceDatabase dao

---
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/dao/onlineInvoice/ListOnlineInvoiceDatabase.java
@@ -0,0 +1,76 @@
+package it.unipd.dei.wa2122.wadteam.dao.onlineInvoice;
+
+import it.unipd.dei.wa2122.wadteam.dao.onlineOrder.GetOnlineOrderByIdDatabase;
+import it.unipd.dei.wa2122.wadteam.resources.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOnlineInvoiceDatabase {
+    private static final String STATEMENT = "SELECT * FROM Online_Invoice";
+
+    /**
+     * The connection to the database
+     */
+    private final Connection con;
+
+    /**
+     * Creates a new object for reading an assistant ticket.
+     *
+     * @param con
+     *            the connection to the database.
+     */
+    public ListOnlineInvoiceDatabase(final Connection con) {
+        this.con = con;
+    }
+
+    /**
+     * Reads an asssistant ticket from the database.
+     *
+     * @return the {@code AssistantTicket} object matching the badge.
+     *
+     * @throws SQLException
+     *             if any error occurs while reading the assistant ticket.
+     */
+    ResultSet resultSet = null;
+
+    public List<OnlineInvoice> getOnlineInvoice() throws SQLException {
+        List<OnlineInvoice> resultOnlineInvoice = new ArrayList<>();
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = con.prepareStatement(STATEMENT);
+
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+
+                int id = resultSet.getInt("ID");
+                int idOrder = resultSet.getInt("ID_Order");
+                String transactionId = resultSet.getString("Transaction_ID");
+                PaymentMethodOnlineEnum paymentType = PaymentMethodOnlineEnum.valueOf(resultSet.getString("Payment_Type "));
+                DateTime date = resultSet.getObject("OI_Date  ", DateTime.class);
+                Double totalPrice=resultSet.getDouble("Total_Price");
+
+                resultOnlineInvoice.add(new OnlineInvoice(id,new GetOnlineOrderByIdDatabase(con,idOrder).getOnlineOrderId(),transactionId,paymentType,date,totalPrice));
+
+            }
+
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+
+            }
+        con.close();
+        return resultOnlineInvoice;
+        }
+    }
