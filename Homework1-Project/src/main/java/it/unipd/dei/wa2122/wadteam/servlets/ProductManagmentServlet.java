0e03446de9378d99cc0066322c12a786536abbb7
Davide <davide.colussi.99@gmail.com>
2022-04-10 20:11:34 +0200

    prepared  ProductManagementServlet for serving different URLs

--- Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/servlets/ProductManagmentServlet.java
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/servlets/ProductManagmentServlet.java
@@ -1,9 +1,7 @@
 package it.unipd.dei.wa2122.wadteam.servlets;
 
 import it.unipd.dei.wa2122.wadteam.dao.product.ListProductDatabase;
-import it.unipd.dei.wa2122.wadteam.resources.Message;
-import it.unipd.dei.wa2122.wadteam.resources.Product;
-import it.unipd.dei.wa2122.wadteam.resources.Resource;
+import it.unipd.dei.wa2122.wadteam.resources.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -18,26 +16,44 @@ public class ProductManagmentServlet extends AbstractDatabaseServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String path = req.getPathInfo() != null ? req.getPathInfo().substring(1).lastIndexOf('/') != -1 ? req.getPathInfo().substring(1,req.getPathInfo().lastIndexOf('/')) : req.getPathInfo().substring(1) : "";
+        String param = req.getPathInfo() != null ? req.getPathInfo().substring(1).lastIndexOf('/') != -1 ? req.getPathInfo().substring(req.getPathInfo().lastIndexOf('/')+1) : "" : "";
 
-        List<Product> products = null;
-
-        try{
-            products = new ListProductDatabase(getDataSource().getConnection()).getProduct();
-
-            List<Resource> lists = new ArrayList<>();
-            for(var prod : products){
-                    lists.add(prod);
-            }
-
-            writeResource(req, res, "/jsp/ProductManagment.jsp", false, lists.toArray(Resource[]::new));
-
-        }catch (SQLException e) {
-            Message m = new Message("Couldn't execute the query", "EU01", e.getMessage());
-            writeError(req, res, m, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        switch (path) {
+            case "" -> getListProduct(req, res);
+            default -> writeError(req, res, new ErrorMessage.IncorrectlyFormattedPathError("page not found"));
         }
+
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws  ServletException, IOException {
         this.doGet(req,res);
     }
+
+    private void getListProduct(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        var ut = ((UserCredential) req.getSession(false).getAttribute("user")).getType();
+        switch (ut) {
+            case CUSTOMER -> writeError(req, res, new ErrorMessage.UserCredentialError("User credential error"));
+            case EMPLOYEE -> {
+                List<Product> products = null;
+
+                try{
+                    products = new ListProductDatabase(getDataSource().getConnection()).getProduct();
+
+                    List<Resource> lists = new ArrayList<>();
+                    for(var prod : products){
+                        lists.add(prod);
+                    }
+
+                    writeResource(req, res, "/jsp/ProductManagment.jsp", false, lists.toArray(Resource[]::new));
+
+                }catch (SQLException e) {
+                    Message m = new Message("Couldn't execute the query", "EU01", e.getMessage());
+                    writeError(req, res, m, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                }
+            }
+
+        }
+    }
+
 }
