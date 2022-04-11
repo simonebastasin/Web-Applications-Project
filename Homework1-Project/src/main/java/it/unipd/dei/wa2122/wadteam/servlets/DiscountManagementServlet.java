c0ad1a7bef300d42db440bdd87a3e4e4bc017f5d
Matteo <matteo.lando@studenti.unipd.it>
2022-04-11 23:38:54 +0200

    Add Discount Servlet and JSP

---
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/servlets/DiscountManagementServlet.java
@@ -0,0 +1,61 @@
+package it.unipd.dei.wa2122.wadteam.servlets;
+
+import it.unipd.dei.wa2122.wadteam.dao.discount.GetDiscountDatabase;
+import it.unipd.dei.wa2122.wadteam.dao.discount.ListDiscountDatabase;
+import it.unipd.dei.wa2122.wadteam.dao.owns.ListProductsFromIdDiscoutDatabase;
+import it.unipd.dei.wa2122.wadteam.dao.product.CreateProductDatabase;
+import it.unipd.dei.wa2122.wadteam.dao.product.ListProductDatabase;
+import it.unipd.dei.wa2122.wadteam.resources.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiscountManagementServlet extends AbstractDatabaseServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String path = req.getPathInfo() != null ? req.getPathInfo().substring(1).lastIndexOf('/') != -1 ? req.getPathInfo().substring(1,req.getPathInfo().lastIndexOf('/')) : req.getPathInfo().substring(1) : "";
+        String param = req.getPathInfo() != null ? req.getPathInfo().substring(1).lastIndexOf('/') != -1 ? req.getPathInfo().substring(req.getPathInfo().lastIndexOf('/')+1) : "" : "";
+
+        switch (path) {
+            case "" -> getListDiscount(req, res);
+            default -> writeError(req, res, new ErrorMessage.IncorrectlyFormattedPathError("page not found"));
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws  ServletException, IOException {
+
+
+    }
+
+    private void getListDiscount(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        List<Discount> discounts = null;
+
+        try{
+            discounts = new ListDiscountDatabase(getDataSource().getConnection()).getDiscounts();
+
+
+            List<Resource> lists = new ArrayList<>();
+            for(var dis : discounts){
+
+                List<Product> products = new ListProductsFromIdDiscoutDatabase(getDataSource().getConnection(), dis).getListProductsFromIdDiscoutDatabase();
+                DiscountListProduct DiscountListProduct = new DiscountListProduct(dis, products);
+
+                lists.add((Resource) DiscountListProduct);
+            }
+
+            writeResource(req, res, "/jsp/discountManagement.jsp", false, lists.toArray(Resource[]::new));
+
+        }catch (SQLException e) {
+            Message m = new Message("Couldn't execute the query", "EU01", e.getMessage());
+            writeError(req, res, m, HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
+
---
+++ Homework1-Project/src/main/webapp/jsp/discountManagement.jsp
@@ -0,0 +1,58 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: matteolando
+  Date: 11/04/2022
+  Time: 19:13
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--@elvariable id="discountList" type="it.unipd.dei.wa2122.wadteam.resources.Discount"--%>
+
+<html>
+<head>
+    <title>Discount List</title>
+</head>
+
+<body>
+
+<c:import url="/jsp/include/header.jsp"/>
+<h1>Discount List</h1>
+
+<table>
+
+    <tr>
+        <th>Id</th>
+        <th>Percentage</th>
+        <th>Start Date</th>
+        <th>End Date</th>
+        <th>Product List</th>
+        <th>Edit</th>
+        <th>Delete</th>
+    </tr>
+
+
+    <c:forEach var="discountListProduct" items="${discountListProductList}">
+
+        <tr>
+            <td>${discountListProduct.discount.id}</td>
+            <td>${discountListProduct.discount.percentage}</td>
+            <td>${discountListProduct.discount.startDate}</td>
+            <td>${discountListProduct.discount.endDate}</td>
+
+            <td>
+            <c:forEach var="prod" items="${discountListProduct.productList}">
+                ${prod.name} <br>
+            </c:forEach>
+            </td>
+
+            <td><button type="button">i</button></td>
+            <td><button type="button">X</button></td>
+        </tr>
+
+    </c:forEach>
+
+</table>
+
+</body>
+</html>
