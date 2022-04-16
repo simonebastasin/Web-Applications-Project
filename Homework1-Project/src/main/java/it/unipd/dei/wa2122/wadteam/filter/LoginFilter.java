1f78a93978de18c02358422d3b7b88af5c767d1d
Gil Czaczkes <gil.czaczkes@studenti.unipd.it>
2022-04-16 12:25:28 +0200

    added LoginFilter

---
+++ Homework1-Project/src/main/java/it/unipd/dei/wa2122/wadteam/filter/LoginFilter.java
@@ -0,0 +1,56 @@
+package it.unipd.dei.wa2122.wadteam.filter;
+
+import it.unipd.dei.wa2122.wadteam.resources.TypeUserEnum;
+import it.unipd.dei.wa2122.wadteam.resources.UserCredential;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class LoginFilter extends AbstractFilter{
+
+    private static final String USER_ATTRIBUTE = "user";
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        if(!(servletRequest instanceof HttpServletRequest) || !(servletResponse instanceof HttpServletResponse)){
+            throw new ServletException("Only HTTP requests/responses are allowed");
+        }
+
+        final HttpServletRequest req = (HttpServletRequest) servletRequest;
+        final HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        HttpSession session = req.getSession(false);
+        String loginURI = req.getContextPath() + "/session/login";
+        String unauthorizedUri = "/jsp/unauthorized.jsp";
+
+        if(session == null){
+            res.sendRedirect(loginURI);
+        }
+        else {
+            final UserCredential user = (UserCredential) session.getAttribute(USER_ATTRIBUTE);
+
+            if(user == null || user.getIdentification().isBlank()){
+                session.invalidate();
+                res.sendRedirect(loginURI);
+            }
+            else{
+                if (user.getType() == TypeUserEnum.EMPLOYEE || user.getType() == TypeUserEnum.CUSTOMER) {
+                    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+                    res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+                    filterChain.doFilter(servletRequest, servletResponse);
+                } else {
+                    res.setStatus(401);
+                    req.getRequestDispatcher(unauthorizedUri).forward(req, res);
+                }
+            }
+        }
+    }
+
+}
